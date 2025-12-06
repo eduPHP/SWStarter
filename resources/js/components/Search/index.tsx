db9effb9129fdf39c80d78edf8015b2cd7293d9c
Edu <edu@rdo.blog.br>
2025-12-06 12:40:02 -0300

    add search component

---
+++ resources/js/components/Search/index.tsx
@@ -0,0 +1,25 @@
+import './styles.scss'
+import { Card } from '@/components/Card';
+import { Input } from '@/components/Input';
+import { Button } from '@/components/Button';
+import { useState } from 'react';
+import { RadioGroup } from '@/components/RadioGroup';
+
+export function Search() {
+    const [search, setSearch] = useState('people');
+    const [selectedOption, setSelectedOption] = useState('people');
+
+    return (
+        <Card className="search">
+            <p>What are you searching for?</p>
+
+            <RadioGroup value={selectedOption} options={[
+                { label: 'People', key: 'people' },
+                { label: 'Movies', key: 'movies' }
+            ]} onSelect={setSelectedOption}/>
+
+            <Input onChange={(ev) => setSearch(ev.target.value)} type="text" placeholder="e.g. Chewbacca, Yoda, Boba Fett"/>
+            <Button disabled={search.length === 0}>Search</Button>
+        </Card>
+    )
+}
