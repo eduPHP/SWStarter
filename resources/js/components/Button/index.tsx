775b089aac83696f3d5d720f1fc41fbfc918f48e
Edu <edu@rdo.blog.br>
2025-12-06 14:22:06 -0300

    improve button

--- resources/js/components/Button/index.tsx
+++ resources/js/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import './styles.scss'
 import React from 'react';
 
-export function Button({ children, onClick, disabled = true }: { children: React.ReactNode, disabled?: boolean, onClick: () => void }) {
+export function Button({ children, onClick, disabled = false }: { children: React.ReactNode, disabled?: boolean, onClick: () => void }) {
     return (
         <button onClick={onClick} className="btn" disabled={disabled}>{children}</button>
     )
